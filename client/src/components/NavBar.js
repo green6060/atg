e203aac3b44e4d7b78a1988a05efb94b897a6503
Dallas Parker <dallasjamesparker@gmail.com>
2018-10-30 18:16:39 -0600

    added announcement to navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -37,6 +37,9 @@ class NavBar extends Component {
           <Link to="/">
             <Menu.Item name="home" />
           </Link>
+          <Link to="/announcements">
+            <Menu.Item name="announcements" />
+          </Link>
           { this.rightNavs() }
         </Menu>
       </div>
