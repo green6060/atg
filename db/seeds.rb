453bd5ae8b407d7b151fc5646d4569da6a64f77f
Dallas Parker <dallasjamesparker@gmail.com>
2018-11-13 16:55:05 -0700

    creating a more sophisticated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,41 @@ require 'faker'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "Creating Admin Accounts..."
+puts "Creating Admin Account..."
 
 @admin = User.create(
   firstName: 'admin',
   lastName: 'admin',
   email: 'admin@admin.com',
   password: 'password',
+  level: 3,
 )
 
-print "Done! Email: admin@admin.com Password: password"
+puts "Done!"
+puts "Test Admin Login Credentials - Email: admin@admin.com, Password: password, Level 3 Access"
+
+puts "Creating Moderator Account..."
+@moderator = User.create(
+  firstName: 'moderator',
+  lastName: 'moderator',
+  email: 'mod@mod.com',
+  password: 'password',
+  level: 2,
+)
+
+puts "Done!"
+puts "Test Moderator Login Credentials - Email: mod@mod.com, Password: password, Level 2 Access"
+
+puts "Creating User Account..."
+@user = User.create(
+  firstName: 'user',
+  lastName: 'user',
+  email: 'user@user.com',
+  password: 'password',
+)
+
+puts "Done!"
+puts "Test User Login Credentials - Email: user@user.com, Password: password, Level 1 Access"
 
 print "Creating announcements..."
 
