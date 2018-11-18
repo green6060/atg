9fd679929ca16a6ee3f5eb308c3c61f11a657f2d
Dallas Parker <dallasjamesparker@gmail.com>
2018-11-18 10:38:28 -0700

    add announcement sorting in controller

--- app/controllers/api/announcements_controller.rb
+++ app/controllers/api/announcements_controller.rb
@@ -2,7 +2,7 @@ class Api::AnnouncementsController < ApplicationController
   before_action :set_announcement, only: [:show, :update, :destroy]
 
   def index
-    render json: Announcement.all
+    render json: Announcement.all.order("created_at DESC")
   end
 
   def show
